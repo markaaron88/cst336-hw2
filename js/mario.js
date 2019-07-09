ab2c84d4a172cbaef61227660dc586fe7146025d
mamariscal <mamariscal@csumb.edu>
2019-07-09 03:02:39 -0400

    changes

--- js/mario.js
+++ js/mario.js
@@ -1,8 +1,51 @@
-function disTxt(){
-  var len = document.myForm.ans1.length;
-  for (i = 0; 0<len;i++){
-    if (document.myForm.ans1[i].checked){
-      
+$("#submitButton" ).click(function() {
+    $("#submitButton").hide();
+    $("#playAgain").show();
+  gamePlay();
+});
+
+$( "#textBoxButton" ).click(function() {
+    var userName = document.forms.gameForm.textBox.value;
+    document.getElementById('name').innerHTML = "PREPARE TO LOSE " + userName + "!-Bowser";
+});
+
+var gameScore = 0;
+
+function gamePlay()
+{
+    if(document.forms.gameForm.ans1.value == "0")
+    {
+        document.getElementById('result1').innerHTML = "Correct!";
+        gameScore++;
+    } else
+    {
+        document.getElementById('result1').innerHTML = "WRONG!";
     }
-  }
+    
+    if(document.forms.gameForm.ans2.value == 0)
+    {
+        document.getElementById('result2').innerHTML = "Correct!";
+        gameScore++;
+    } else
+    {
+        document.getElementById('result2').innerHTML = "PRINCESS PEACH IS MINE!";
+    }
+    
+  
+  
+    if(document.forms.gameForm.ans3.value == 4)
+    {
+        document.getElementById('result3').innerHTML = "Correct!";
+        gameScore++;
+    } else
+    {
+        document.getElementById('result3').innerHTML = "TAKE THAT L!";
+    }
+    
+     document.getElementById('showScore').innerHTML = "Your score is " + gameScore + ".";
+     
 }
+
+
+
+
